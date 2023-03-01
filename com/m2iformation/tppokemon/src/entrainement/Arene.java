d683788ff5545e31bf6329fd1fbca2d3f0873172
Anais1805 <111952504+Anais1805@users.noreply.github.com>
2023-03-01 17:09:41 +0100

    arene+areneEnum

---
+++ com/m2iformation/tppokemon/src/entrainement/Arene.java
@@ -0,0 +1,26 @@
+package entrainement;
+public class Arene {
+    private String nom;
+    private int effetUnique;
+    private int effetPermanent;
+    
+    public String getNom() {
+        return nom;
+    }
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public int getEffetUnique() {
+        return effetUnique;
+    }
+    public void setEffetUnique(int effetUnique) {
+        this.effetUnique = effetUnique;
+    }
+    public int getEffetPermanent() {
+        return effetPermanent;
+    }
+    public void setEffetPermanent(int effetPermanent) {
+        this.effetPermanent = effetPermanent;
+    }
+}
