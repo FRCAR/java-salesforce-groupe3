af9302e9ac96fb60646f2d14de0d7f07c980de7d
Mohssine Zahri <zahri.mohssine@gmail.com>
2023-03-02 09:20:34 +0100

    ajout Referentiel et Attaque

---
+++ com/m2iformation/tppokemon/src/referentiel/Attaque.java
@@ -0,0 +1,24 @@
+package referentiel;
+
+public class Attaque {
+
+    private final String nom;
+    private int degats;
+
+    public String getNom() {
+        return nom;
+    }
+
+    public int getDegats() {
+        return degats;
+    }
+
+    public void setDegats(int degats) {
+        this.degats = degats;
+    }
+
+    public int calculDegats() {
+
+    }
+
+}
---
+++ com/m2iformation/tppokemon/src/referentiel/Referentiel.java
@@ -0,0 +1,26 @@
+package referentiel;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+public class Referentiel {
+    Map<Integer, Espece> map = new HashMap<>();
+
+    public void RecupAllPoke() {
+        Iterator iterator = map.entrySet().iterator();
+        while (iterator.hasNext()) {
+            Map.Entry map = (Map.Entry) iterator.next();
+            map.getKey();
+            map.getValue();
+        }
+    }
+
+    public void RecupPokeParId(int id) {
+        map.get(id);
+    }
+
+    public void updatePoke(Espece espece) {
+
+    }
+}
